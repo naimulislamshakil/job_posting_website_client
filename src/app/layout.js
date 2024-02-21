3b6e20ba5910d573e2426845dd05017c34f75a2d
Naimul Islam <ms0413354@gmail.com>
2024-02-21 15:38:42 +0600

    fix error 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
 				<Navbar />
 				{children}
 				<Footer />
-				<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+				{/* <script src="../path/to/flowbite/dist/flowbite.min.js"></script> */}
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 			</body>
 		</html>
