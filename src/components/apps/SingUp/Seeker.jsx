245b7aaf521b0565b9940ff5153ee7b21d739c15
Naimul Islam <ms0413354@gmail.com>
2024-03-04 18:31:53 +0600

    almost done seeker singup section

--- src/components/apps/SingUp/Seeker.jsx
+++ src/components/apps/SingUp/Seeker.jsx
@@ -1,11 +1,24 @@
 'use client';
-import seeker from '@/config/App/SingUp/Seeker';
-import React, { useState } from 'react';
+import seeker, {
+	availability,
+	current_job_function,
+	highest_qualification,
+	jobFunction,
+	years_of_experience,
+} from '@/config/App/SingUp/Seeker';
+import React, { useEffect, useState } from 'react';
 import PasswordStrength from './PasswordStrength';
 import Link from 'next/link';
 
 const Seeker = () => {
 	const [password, setPassword] = useState('');
+	const job = [];
+
+	const add = (cate) => {
+		job.push(cate);
+		console.log(job.length);
+		// console.log(cate);
+	};
 
 	return (
 		<div className="container mx-auto mt-28">
@@ -262,6 +275,227 @@ const Seeker = () => {
 									class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
 								>
 									Submit
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<h2 className="text-center text-2xl text-gray-500 my-4 font-bold">
+				{seeker?.step2}
+			</h2>
+
+			{/* Create a card for step 2 */}
+
+			<div className="bg-gray-300 rounded p-5 mt-5 w-full">
+				<div className="grid md:grid-cols-4 grid-cols-1 gap-2">
+					<div>
+						<h4 className="text-lg font-semibold text-gray-700">
+							{seeker?.work}
+						</h4>
+						<p className="pr-3 text-lg mt-3 text-gray-700">
+							{seeker?.workDetils}
+						</p>
+					</div>
+					<div className="col-span-3">
+						{/* Create a card for from */}
+
+						<div class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+							<form>
+								{/* Highest Qualification */}
+
+								<div class="grid gap-6 mb-6 md:grid-cols-2">
+									<div>
+										<label
+											for="hq"
+											class="block mb-2 text-sm font-medium text-gray-900"
+										>
+											{seeker?.Highest_Qualification}
+										</label>
+										<select
+											id="hq"
+											class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+										>
+											{highest_qualification?.map((edu, i) => (
+												<option key={i} value={edu}>
+													{edu}
+												</option>
+											))}
+										</select>
+									</div>
+									<div>
+										<label
+											for="experience"
+											class="block mb-2 text-sm font-medium text-gray-900"
+										>
+											{seeker?.years_of_experience}
+										</label>
+										<select
+											id="experience"
+											class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+										>
+											{years_of_experience?.map((years, i) => (
+												<option key={i} value={years}>
+													{years}
+												</option>
+											))}
+										</select>
+									</div>
+								</div>
+
+								{/* Nationlity, Gender, Location */}
+								<div class="grid gap-6 mb-6 md:grid-cols-2">
+									<div>
+										<label
+											for="current"
+											class="block mb-2 text-sm font-medium text-gray-900"
+										>
+											{seeker?.current_job_function}
+										</label>
+										<select
+											id="current"
+											class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+										>
+											{current_job_function?.map((fun, i) => (
+												<option key={i} value={fun}>
+													{fun}
+												</option>
+											))}
+										</select>
+									</div>
+
+									<div>
+										<label
+											for=""
+											class="block mb-2 text-sm font-medium text-gray-900"
+										>
+											Location
+										</label>
+
+										{/* dropdown */}
+
+										<button
+											id="dropdownSearchButton"
+											data-dropdown-toggle="dropdownSearch"
+											class="inline-flex justify-between items-center text-sm font-medium text-center bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
+											type="button"
+										>
+											Select 3...
+											<svg
+												class="w-2.5 h-2.5 ms-2.5"
+												aria-hidden="true"
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 10 6"
+											>
+												<path
+													stroke="currentColor"
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													stroke-width="2"
+													d="m1 1 4 4 4-4"
+												/>
+											</svg>
+										</button>
+
+										{/* <!-- Dropdown menu --> */}
+										<div
+											id="dropdownSearch"
+											class="z-10 hidden bg-white rounded-lg shadow w-[30%]"
+										>
+											<div class="p-3">
+												<label for="input-group-search" class="sr-only">
+													Search
+												</label>
+												<div class="relative">
+													<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+														<svg
+															class="w-4 h-4 text-gray-500"
+															aria-hidden="true"
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 20 20"
+														>
+															<path
+																stroke="currentColor"
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																stroke-width="2"
+																d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+															/>
+														</svg>
+													</div>
+													<input
+														type="text"
+														id="input-group-search"
+														class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+														placeholder="Search user"
+													/>
+												</div>
+											</div>
+											<ul
+												class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700"
+												aria-labelledby="dropdownSearchButton"
+											>
+												{jobFunction?.map((cate, i) => (
+													<li onClick={(e) => add(cate)}>
+														<div class="flex items-center p-2 rounded hover:bg-gray-100">
+															<input
+																id="checkbox-item-11"
+																type="checkbox"
+																value=""
+																class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2"
+															/>
+															<label
+																for="checkbox-item-11"
+																class="w-full ms-2 text-sm font-medium text-gray-900 rounded0"
+															>
+																{cate}
+															</label>
+														</div>
+													</li>
+												))}
+											</ul>
+										</div>
+									</div>
+								</div>
+
+								{/* Availability */}
+
+								<div class="grid gap-6 mb-6 md:grid-cols-1">
+									<div>
+										<label
+											for="availability"
+											class="block mb-2 text-sm font-medium text-gray-900"
+										>
+											{seeker?.availability}
+										</label>
+										<select
+											id="availability"
+											class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+										>
+											{availability?.map((time, i) => (
+												<option key={i} value={time}>
+													{time}
+												</option>
+											))}
+										</select>
+									</div>
+								</div>
+
+								{/* File Upload */}
+								<input
+									class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 mb-4"
+									id="file_input"
+									type="file"
+								></input>
+
+								<button
+									type="submit"
+									class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+								>
+									Create Your Account
 								</button>
 							</form>
 						</div>
--- src/config/App/SingUp/Seeker.js
+++ src/config/App/SingUp/Seeker.js
@@ -2,10 +2,102 @@ const seeker = {
 	h4: 'Create a Job Seeker Account',
 	p: 'Your new career is one click away',
 	step1: 'STEP 1 of 2',
+	step2: 'STEP 2 of 2',
 	Personal: 'Personal Information',
+	work: 'Work Information',
 	PersonalDetils: 'This is information pertaining to you as an individual',
+	workDetils:
+		'This is information pertaining to your past work experience and your qualifications',
 	question: 'Already have an account?',
 	login: 'Log in',
+	Highest_Qualification: 'Highest Qualification',
+	years_of_experience: 'Years of Experience',
+	current_job_function: 'Current Job Function',
+	availability: 'Availability',
 };
+
+export const highest_qualification = [
+	'Select...',
+	'Degree',
+	'Diploma',
+	'High School (S.S.C.E)',
+	'HND',
+	'MBA/MSc',
+	'MBBS',
+	'MPhil/PhD',
+	'N.C.E',
+	'OND',
+	'Others',
+	'Vocational',
+];
+
+export const years_of_experience = [
+	'Select...',
+	'No Experience/ Less than 1 year',
+	'1 Year',
+	'2 Years',
+	'3 Years',
+	'4 Years',
+	'5 Years',
+	'6 Years',
+	'7 Years',
+	'8 Years',
+	'9 Years',
+	'10 Years',
+	'11 Years',
+	'12 Years',
+	'13 Years',
+	'14 Years',
+	'15 Years',
+	'16 Years',
+	'17 Years',
+	'18 Years',
+	'19 Years',
+	'20 Years',
+	'More than 20 Years',
+];
+
+export const current_job_function = [
+	'Select...',
+	'Computer Scientist',
+	'IT Professional',
+	'UX Designer & UI Developer',
+	'SQL Developer',
+	'Web Designer',
+	'Web Developer',
+	'Help Desk Worker/Desktop Support',
+	'Software Engineer',
+	'Data Entry',
+	'DevOps Engineer',
+	'Computer Programmer',
+	'Network Administrator',
+	'Information Security Analyst',
+	'Artificial Intelligence Engineer',
+	'Cloud Architect',
+	'IT Manager',
+	'Technical Specialist',
+	'Application Developer',
+	'Chief Technology Officer (CTO)',
+	'Chief Information Officer (CIO)',
+];
+
+export const jobFunction = [
+	'Accounting, Auditing & Finance',
+	'Admin & Office',
+	'Building & Architecture',
+	'Consulting & Strategy',
+	'Creative & Design',
+];
+
+export const availability = [
+	'Select...',
+	'Immediately',
+	'1 Week',
+	'2 Weeks',
+	'3 Weeks',
+	'1 Month',
+	'2 Months',
+	'More than 3 Months',
+];
 
 export default seeker;
